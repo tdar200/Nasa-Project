95301a00afbb136e6e2b8e29893ca0a8b6ce1ca9
Tahseen <tahseendar01@gmail.com>
2022-03-21 11:00:50 +0500

    csv parser

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const parse = require("csv-parse");
+const { parse } = require("csv-parse");
 const fs = require("fs");
 
 const results = [];
 
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
 fs.createReadStream("kepler_data.csv")
+
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      results.push(data);
+    }
   })
   .on("error", (err) => {
     console.error(err);
   })
   .on("end", () => {
-    console.log(results);
+    console.log(results.map((planet) => planet["kepler_name"]));
   });
 
 // parse()
